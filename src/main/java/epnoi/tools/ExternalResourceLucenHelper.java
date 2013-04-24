cec06152e975a27807efaf3333fa26b3069cd112
Rafael Gonzalez Cabero <rafael.gonzalez.cabero@gmail.com>
2013-04-25 00:32:25 +0200

    First version of the new tools code

---
+++ src/main/java/epnoi/tools/ExternalResourceLucenHelper.java
@@ -0,0 +1,9 @@
+package epnoi.tools;
+
+public class ExternalResourceLucenHelper {
+	public final static String TITLE ="title";
+	public final static String CONTENT ="contents";
+	public final static String URI = "uri";
+	public final static String AUTHOR = "author";
+		
+}
---
+++ src/main/java/epnoi/tools/logging/EpnoiToolsLogger.java
@@ -0,0 +1,43 @@
+package epnoi.tools.logging;
+
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Formatter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class EpnoiToolsLogger {
+	static private FileHandler fileTXT;
+	static private SimpleFormatter formatterTXT;
+
+	static private FileHandler fileHTML;
+	static private Formatter formatterHTML;
+
+	static public void setup(String path) {
+		// Create Logger
+		System.out.println("path -----> entra "+ path);
+		Logger logger = Logger.getLogger("");
+		logger.setLevel(Level.INFO);
+		try {
+			fileTXT = new FileHandler(path + "log.txt");
+			fileHTML = new FileHandler(path + "log.html");
+		} catch (SecurityException e) {
+
+			e.printStackTrace();
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+
+		// Create txt Formatter
+		formatterTXT = new SimpleFormatter();
+		fileTXT.setFormatter(formatterTXT);
+		logger.addHandler(fileTXT);
+
+		// Create HTML Formatter
+		formatterHTML = new HtmlFormatter();
+		fileHTML.setFormatter(formatterHTML);
+		logger.addHandler(fileHTML);
+	}
+}
